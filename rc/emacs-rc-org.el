ae70282a98796deb9e6343f5cfe29a9702d58d89
Sergei Lebedev <superbobry@gmail.com>
2010-08-11 21:16:53 +0400

    More org-mode magic
      - Added fixes for conflicting packages: windmove, yasnippet
      - Added org-habit and org-clock
      - Changed todo-keywords workflow

--- rc/emacs-rc-org.el
+++ rc/emacs-rc-org.el
@@ -7,14 +7,18 @@
 (setq org-agenda-files `(,(concat org-directory "gtd.org"))
       org-agenda-skip-deadline-if-done t
       org-agenda-skip-scheduled-if-done t
+      org-completion-use-ido t
+      org-deadline-warning-days 7
       org-default-notes-file (concat org-directory "journal.org")
       org-hide-leading-stars t
       org-log-done t
       org-fast-tag-selection-single-key 'expert
       org-return-follows-link t
       org-reverse-note-order t
-      org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "STARTED(s)" "|"
-                                    "DONE(d)" "CANCELED(c)")))
+      org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+        (sequence "WAITING(w@/!)" "SOMEDAY(s!)" "|" "CANCELLED(c@/!)"))
+      org-use-fast-todo-selection t)
 
 (add-to-list 'org-link-abbrev-alist '("emacswiki" . "http://www.emacswiki.org/cgi-bin/wiki/"))
 (add-to-list 'org-link-abbrev-alist '("google" . "http://www.google.com/search?q="))
@@ -22,6 +26,7 @@
 
 ;; Remember
 (require 'remember)
+(require 'org-remember)
 
 (org-remember-insinuate)
 (setq remember-annotation-functions '(org-remember-annotation)
@@ -56,12 +61,41 @@ Comments:\n" nil "Music")
 :END:
 
 Comments: \n" nil "Movies")
-        ("Idea" ?i "** %^{Idea} %U\n%? :IDEA:%^g \n" nil "Ideas")
-        ("Word" ?w "** %^{Word} %U\n%? :WORD: \n" nil "Words")
+        ("Word" ?w "** /%^{Word}/ %U :WORD:
+%?
+
+Example:
+" nil "Words")
+        ("Idea" ?i "** %^{Idea} %U :IDEA:%^g \n%?" nil "Ideas")
         ("Buzz" ?b "** %^{Topic} %T %^g \n%i%?\n" nil "Buzz")
       ))
 
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+;; Clock
+(require 'org-clock)
+(org-clock-persistence-insinuate)
+(setq org-clock-idle-time 10)
+(setq org-clock-out-remove-zero-time-clocks t)
+(setq org-clock-persist 'history)
+
+;; Habit
+(require 'org-habit)
+(add-to-list 'org-modules 'org-habit)
+(setq org-global-properties (quote (("STYLE_ALL" . "habit")))
+      org-habit-graph-column 50)
+
+;; Compatibility issues.
+;; a) windmove
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+;; b) yasnippet
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-set-local 'yas/trigger-key [tab])
+            (define-key yas/keymap [tab] 'yas/next-field-group)))
 
 ;; Bindings
 (global-set-key (kbd "\C-cl") 'org-store-link)
