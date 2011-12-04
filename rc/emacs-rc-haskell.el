ae932000cbdf73ad4aa1e438ae8d8b87ccc57ac2
Sergei Lebedev <superbobry@gmail.com>
2011-12-04 22:58:48 +0400

    Updated `haskell-mode` autoloads -- now with ghci and hlint!

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -2,12 +2,15 @@
 
 
 (when (load "haskell-site-file.el" t nil nil)
+  (require 'inf-haskell)
+  (require 'haskell-checkers)
+  (require 'haskell-ghci)
+
   (add-to-list 'auto-mode-alist  '("\\.hs$" . haskell-mode))
 
   (add-hook 'haskell-mode-hook 'run-coding-hook)
-  (add-hook 'haskell-mode-hook '(lambda ()
-				  (turn-on-haskell-doc-mode)
-				  (turn-on-haskell-indent))))
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
 
 
 ;;; emacs-rc-haskell.el ends here
