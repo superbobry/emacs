3f3ad40af77872c06a939ab515d7e6106c48c56e
Sergei Lebedev <superbobry@gmail.com>
2012-12-06 23:50:27 +0400

    Finally solved the issue with '$PATH' on OS X

--- rc/emacs-rc-osx.el
+++ rc/emacs-rc-osx.el
@@ -4,12 +4,12 @@
 (custom-set-variables
  '(shell-file-name "~/.homebrew/bin/zsh"))
 
-(if (not (getenv "TERM_PROGRAM"))
-    (setenv "PATH"
-            (shell-command-to-string
-             "source $HOME/.zshrc > /dev/null && printf $PATH")))
+(when (not (getenv "TERM_PROGRAM"))
+  (setenv "PATH"
+          (shell-command-to-string
+           "source $HOME/.zshrc > /dev/null && printf $PATH"))
+  (add-to-list 'exec-path "~/.homebrew/bin"))
 
-(add-to-list 'load-path "~/.homebrew/lib/erlang/lib/tools-2.6.7/emacs")
 (add-to-list 'load-path "~/.homebrew/share/emacs/site-lisp")
 
 
