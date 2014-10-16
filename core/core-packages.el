1ec04b1467015b3a2e96417f9bf81bcbf6ce0507
Sergei Lebedev <superbobry@gmail.com>
2014-10-16 17:34:10 +0400

    ... and removed it from 'core-packages'

--- core/core-packages.el
+++ core/core-packages.el
@@ -3,8 +3,12 @@
 (require 'cl)
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa.milkbox.net/packages/")
+             t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/")
+             t)
 
 (setq package-user-dir (local-file-name "elpa"))
 
@@ -14,7 +18,7 @@
 (package-initialize)
 
 (defvar bobry-packages
-  '(dash diminish exec-path-from-shell yasnippet dropdown-list use-package)
+  '(dash diminish exec-path-from-shell dropdown-list use-package)
   "A list of packages to ensure are installed at launch.")
 
 (mapc #'(lambda (package)
